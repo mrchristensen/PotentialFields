4b6724853f215c8468bd278fd15a765924bcc469
Matt Christensen <mr.christensen@gmail.com>
2022-01-21 19:10:39 -0700

    Removed unused c files

--- Viewer/pfields_cpp.sh
+++
@@ -1,6 +0,0 @@
-cd ../MundoVerdadero/SimCode
-./sim > simOutput.txt
-cd ../../Robot
-./robot pfields false > robotOutput.txt
-cd ../AgentCpp
-./agentCpp pfields false > agentOutput.txt
--- Viewer/simulator_on_cpp.sh
+++
@@ -1,6 +0,0 @@
-cd ../Robot
-./robot false navigate > robotOutput.txt &
-cd ../AgentCpp
-./agentCpp false navigate > agentOutput.txt &
-cd ../MundoVerdadero/SimCode
-./sim > simOutput.txt &
